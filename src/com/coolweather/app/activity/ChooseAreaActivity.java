684a7a5ecdd82828acf77f0e957ad64b5d71a5f0
sjm <1217594215@qq.com>
2016-02-03 16:30:40 +0800

    加入显示天气信息的功能。

--- src/com/coolweather/app/activity/ChooseAreaActivity.java
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -72,6 +72,15 @@ public class ChooseAreaActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		 SharedPreferences prefs =PreferenceManager.getDefaultSharedPreferences(this);
+	     if(prefs.getBoolean("city_selected", false)){
+	    	 Intent intent=new Intent(this,WeatherActivity.class);
+	    	 startActivity(intent);
+	    	 finish();
+	    	 return;
+	     }
+		
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.choose_area);
 		listView = (ListView) findViewById(R.id.list_view);
@@ -79,16 +88,25 @@ public class ChooseAreaActivity extends Activity {
 		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataList);
 		listView.setAdapter(adapter);
 		coolWeatherDB = CoolWeatherDB.getInstance(this);
+		
+	   
+	    
 		listView.setOnItemClickListener(new OnItemClickListener() {
 		   @Override
 		   public void onItemClick(AdapterView<?> arg0, View view, int index,long arg3) {
-		    if (currentLevel == LEVEL_PROVINCE) {
+		     if (currentLevel == LEVEL_PROVINCE) {
 		         selectedProvince = provinceList.get(index);
 		         queryCities();
-		    } else if (currentLevel == LEVEL_CITY) {
+		     }else if (currentLevel == LEVEL_CITY) {
 		                selectedCity = cityList.get(index);
 		         queryCounties();
-				}
+			 }else if(currentLevel==LEVEL_COUNTY){
+				    String countyCode=countyList.get(index).getCountyCode();
+				    Intent intent=new Intent(ChooseAreaActivity.this,WeatherActivity.class);
+				    intent.putExtra("county_code", countyCode);
+				    startActivity(intent);
+				    finish();
+			  }
 			}
 		});
 		queryProvinces();  // 加载省级数据
